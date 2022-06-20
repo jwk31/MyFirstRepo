441709024f262c7a7ad7c7d30bbd9295bae24606
Yrysbek Dzharkombaev <jwk103110@gmail.com>
2022-06-20 19:44:37 +0600

    test

--- src/main/java/javafaker/HelloWorld.java
+++ src/main/java/javafaker/HelloWorld.java
@@ -2,6 +2,8 @@ package javafaker;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Test");
     }
 }
